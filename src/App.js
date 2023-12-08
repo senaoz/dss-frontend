0fe49c96a500373c686a649fd1a0bf9e2cb37bb8
Sena Oz <tazeyta@gmail.com>
2023-12-08 16:30:49 +0300

    update home page

--- src/App.js
+++ src/App.js
@@ -1,48 +1,94 @@
 import House from "./components/house";
+import HouseCard from "./components/houseCard";
+
+const navigation = [
+  { name: "Product", href: "#" },
+  { name: "Features", href: "#" },
+  { name: "Marketplace", href: "#" },
+  { name: "Company", href: "#" },
+];
 
 function App() {
   return (
     <div className="prose container mx-auto p-8 max-w-6xl space-y-4">
-      <header className="flex justify-between items-center">
-        <h1 className=" font-normal">
-          Smart<b>Housing</b>Insights
-        </h1>
-        <nav className="space-x-4">
-          <a href="#" className="uppercase ">
-            Button
-          </a>
-          <a href="#" className="uppercase">
-            Button
-          </a>
-          <a href="#" className="uppercase button">
-            Button
-          </a>
-        </nav>
-      </header>
-      <main>
-        <div
-          className="flex justify-between items-center p-4 rounded-lg bg-[url('./assets/images/banner.png')]"
-          id="hero"
-        >
-          <h1 className="text-4xl font-bold">Smart Housing Insights</h1>
-          <a href="#" className="uppercase button">
-            Button
-          </a>
+      <div className="bg-white">
+        <header className="flex justify-between items-center absolute inset-x-0 top-0 z-50 p-10">
+          <h2 className="font-normal">
+            Smart<b>Housing</b>Insights
+          </h2>
+          <nav className="space-x-4">
+            {navigation.map((item) => (
+              <a key={item.name} href={item.href} className="uppercase">
+                {item.name}
+              </a>
+            ))}
+
+            <a href="#" className="uppercase button">
+              Button
+            </a>
+          </nav>
+        </header>
+
+        <div className="relative isolate px-6 lg:px-8 z-10">
+          <div
+            className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+            aria-hidden="true"
+          >
+            <div
+              className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+              style={{
+                clipPath:
+                  "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+              }}
+            />
+          </div>
+          <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+            <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+              <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/30">
+                Announcing our methods for the price predictions.{" "}
+                <a href="#" className="font-semibold">
+                  <span className="absolute inset-0" aria-hidden="true" />
+                  Read more <span aria-hidden="true">&rarr;</span>
+                </a>
+              </div>
+            </div>
+            <div className="text-center">
+              <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+                Easy way to find a perfect price for your property
+              </h1>
+              <p className="mt-6 text-lg leading-8 text-gray-600">
+                We are using linear regression to predict the price of your
+                property based on the data we have collected from the market
+                with more than 21,000 data points.
+              </p>
+              <div className="mt-10 flex items-center justify-center gap-x-6">
+                <a href="#" className="button">
+                  Get started
+                </a>
+                <a href="#" className="font-semibold">
+                  Learn more <span aria-hidden="true">→</span>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+            aria-hidden="true"
+          >
+            <div
+              className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+              style={{
+                clipPath:
+                  "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+              }}
+            />
+          </div>
         </div>
-        <h2 className="text-2xl font-bold">
-          Welcome to Smart Housing Insights
-        </h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-          eleifend, augue eu vulputate lacinia, ipsum sapien ultricies dolor, ut
-          mollis diam risus quis purus. Nulla facilisi. Donec vitae velit sed
-          dolor aliquet ultricies. Donec euismod ullamcorper mauris, non luctus
-          nunc. Sed euismod, nisl quis lacinia dapibus, ipsum velit ultricies
-          libero
-        </p>
-      </main>
+      </div>
 
       <House />
+
+      <HouseCard />
     </div>
   );
 }
