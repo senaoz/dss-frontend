75921389fedfa1c38ee5702899e82badf347ca61
Sena Oz <tazeyta@gmail.com>
2023-12-08 15:28:33 +0300

    version 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import House from "./components/house";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="prose container mx-auto p-8 max-w-6xl space-y-4">
+      <header className="flex justify-between items-center">
+        <h1 className=" font-normal">
+          Smart<b>Housing</b>Insights
+        </h1>
+        <nav className="space-x-4">
+          <a href="#" className="uppercase ">
+            Button
+          </a>
+          <a href="#" className="uppercase">
+            Button
+          </a>
+          <a href="#" className="uppercase button">
+            Button
+          </a>
+        </nav>
       </header>
+      <main>
+        <div
+          className="flex justify-between items-center p-4 rounded-lg bg-[url('./assets/images/banner.png')]"
+          id="hero"
+        >
+          <h1 className="text-4xl font-bold">Smart Housing Insights</h1>
+          <a href="#" className="uppercase button">
+            Button
+          </a>
+        </div>
+        <h2 className="text-2xl font-bold">
+          Welcome to Smart Housing Insights
+        </h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+          eleifend, augue eu vulputate lacinia, ipsum sapien ultricies dolor, ut
+          mollis diam risus quis purus. Nulla facilisi. Donec vitae velit sed
+          dolor aliquet ultricies. Donec euismod ullamcorper mauris, non luctus
+          nunc. Sed euismod, nisl quis lacinia dapibus, ipsum velit ultricies
+          libero
+        </p>
+      </main>
+
+      <House />
     </div>
   );
 }
